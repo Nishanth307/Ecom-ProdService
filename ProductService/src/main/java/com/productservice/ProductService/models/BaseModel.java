d8f08b82343654200857ff51b9f049e1e9972b4d
Nishanth <nishanthprabhu0@gmail.com>
2024-10-26 16:07:40 +0530

    UUID generation

--- ProductService/src/main/java/com/productservice/ProductService/models/BaseModel.java
+++ ProductService/src/main/java/com/productservice/ProductService/models/BaseModel.java
@@ -1,5 +1,9 @@
 package com.productservice.ProductService.models;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
@@ -10,5 +14,8 @@ import lombok.Setter;
 @MappedSuperclass
 public class BaseModel{
       @Id
+      @GeneratedValue(generator = "generator_name")
+      //@GenericGenerator(name = "generator_name", strategy = "uuid2")
+      @Column(name = "id", columnDefinition = "binary(16)", nullable = false, updatable = false)
       private Long id;
 }
